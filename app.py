a6e252133f8445f8d5f54c0f5533b91de89b5dc0
daydash <thedaydash@gmail.com>
2023-12-06 18:27:47 +0530

    trying pyodide

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from splinter import Browser
-# import time
-from flask import Flask
+import time
+from flask import Flask, render_template, render_template_string
 # ,request, jsonify
 from waitress import serve
 from flask_cors import CORS
@@ -19,6 +19,48 @@ def hello_world():
     logging.info("HELLO, from /")
     return "<p>Hello, World!!</p>"
 
+@app.route("/search3")
+def search3():
+    script_to_inject = """
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/pyodide@0.24.1/pyodide.min.js"></script>
+        <script type="text/javascript">
+        console.log("Before using languagePluginLoader");
+        loadPyodide({ indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.0/full" }).then((pyodide) => {
+            console.log("inside lang")
+            globalThis.pyodide = pyodide 
+            pyodide.loadPackage(['selenium','splinter']).then(() => {
+            const result = pyodide.runPython(`
+                from selenium import webdriver
+                import time
+                from selenium.webdriver.chrome.options import Options
+                import logging
+
+                try:
+                    
+                    chrome_options = Options()
+                    driver = webdriver.Chrome(options=chrome_options)
+                    data = driver.get('https://www.google.com')
+                
+
+                    search_box = driver.find_element('name','q')
+                
+                    search_box.send_keys("chatgpt")
+                 
+                    search_box.submit()
+     
+                    time.sleep(5)
+                except:
+                    logging.warning('in expect\n')
+            `);
+            console.log(result);
+            });
+        });
+        console.log("After using languagePluginLoader");
+        </script>
+    """
+    return render_template_string("<html><body>{{ script_to_inject | safe}}</body></html>", script_to_inject=script_to_inject)
+
+
 
 @app.route("/search2")
 def search2():
@@ -32,7 +74,7 @@ def search2():
         driver = webdriver.Chrome(options=chrome_options)
         logging.warning("search2 3\n")
         # driver = webdriver.Chrome()
-        logging.critical(driver.get("https://www.google.com"))
+        data = driver.get("https://www.google.com")
         logging.warning("search2 4\n")
 
         search_box = driver.find_element("name","q")
@@ -41,9 +83,11 @@ def search2():
         logging.warning("search2 6\n")
         search_box.submit()
         logging.warning("search2 7\n")
+        time.sleep(5)
     except:
         logging.warning("in expect\n")
-    return "hello"
+   
+    return "hello form search2 "
 
 @app.route("/search")
 def search():
@@ -66,6 +110,7 @@ def search():
     browser.quit()
 
     return "hello from search route"
+    # return render_template('index.html')
 
 # @app.route("/loginToWW", methods=["POST"])
 # @cross_origin()
@@ -108,7 +153,7 @@ def search():
 
 #     return username
 
-mode = "prod"
+mode = "dev"
 
 if __name__ == "__main__":
     if mode == "prod":
