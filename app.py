a9993c24a1f343008fce7396347a831b879b5c51
daydash <thedaydash@gmail.com>
2023-12-06 11:59:27 +0530

    added search route

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from selenium import webdriver
+# from selenium import webdriver
 from splinter import Browser
-import time
-from flask import Flask,request, jsonify
+# import time
+from flask import Flask
+# ,request, jsonify
 from waitress import serve
 from flask_cors import CORS
 
@@ -13,46 +14,67 @@ cors = CORS(app)
 def hello_world():
     return "<p>Hello, World!!</p>"
 
-@app.route("/loginToWW", methods=["POST"])
+
+@app.route("/search")
+def search():
+    from splinter import Browser
+
+    browser = Browser()
+
+    browser.visit('http://google.com')
+    input_element = browser.find_by_name('q')
+    input_element.fill('splinter - python acceptance testing for web applications')
+
+    button_element = browser.find_by_name('btnK')[1]
+    button_element.click()
+
+    if browser.is_text_present('splinter.readthedocs.io'):
+        print("Yes, the official website was found!")
+    else:
+        print("No, it wasn't found... We need to improve our SEO techniques")
+
+    browser.quit()
+
+# @app.route("/loginToWW", methods=["POST"])
 # @cross_origin()
-def login():
-    username = request.json['username']
-    password = request.json['password']
+# def login():
+#     username = request.json['username']
+#     password = request.json['password']
 
-    print(username, password)
+#     print(username, password)
 
-    try:
-        # options = webdriver.ChromeOptions()
-        # options.add_argument('--headless') 
-        # options.add_argument('--disable-gpu')
-        # driver = webdriver.Chrome(options=options)
+#     try:
+#         # options = webdriver.ChromeOptions()
+#         # options.add_argument('--headless') 
+#         # options.add_argument('--disable-gpu')
+#         # driver = webdriver.Chrome(options=options)
 
-        browser = Browser()
-        browser.visit('https://vaishu-15.github.io/WealthWise/')
-    except Exception as error:
-        print("Error in Browser instance ->\n", error)
+#         browser = Browser()
+#         browser.visit('https://vaishu-15.github.io/WealthWise/')
+#     except Exception as error:
+#         print("Error in Browser instance ->\n", error)
 
-    getStarted = browser.links.find_by_partial_href('login.html')
-    getStarted.click()
+#     getStarted = browser.links.find_by_partial_href('login.html')
+#     getStarted.click()
 
-    time.sleep(1)
-    emailField = browser.find_by_name("logemail")
-    emailField.fill(username)
-    passwordField = browser.find_by_name("logpass")
-    passwordField.fill(password)
+#     time.sleep(1)
+#     emailField = browser.find_by_name("logemail")
+#     emailField.fill(username)
+#     passwordField = browser.find_by_name("logpass")
+#     passwordField.fill(password)
 
-    time.sleep(1)
-    submit = browser.find_by_id('submitButton')
-    submit.click()
+#     time.sleep(1)
+#     submit = browser.find_by_id('submitButton')
+#     submit.click()
 
-    time.sleep(1)
-    submitAgain = browser.find_by_id('submitButton')
-    submitAgain.click()
+#     time.sleep(1)
+#     submitAgain = browser.find_by_id('submitButton')
+#     submitAgain.click()
 
-    time.sleep(1)
-    browser.execute_script("document.getElementById('submitButton').click()")
+#     time.sleep(1)
+#     browser.execute_script("document.getElementById('submitButton').click()")
 
-    return username
+#     return username
 
 mode = "prod"
 
